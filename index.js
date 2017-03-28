ebe84826c2ddd407dc48e8e934c558a39c61268e
Kirill Izotov <enykeev@stackstorm.com>
2017-03-28 15:20:16 +0700

    preload the files from public folder

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 
 /* eslint no-param-reassign:off */
 
+const preload = require('./lib/preload');
 const process = require('./lib/process');
 const render = require('./lib/render');
 const serve = require('./lib/serve');
 
 const files = {};
 
+preload(files);
 process(files);
 render(files);
 serve(files);
